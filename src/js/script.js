40f21b1fb1d86991c113b2a199eff41565742d2e
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-01-10 19:23:59 -0300

    feat

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const nome = document.querySelector('#name');
+const snome = document.querySelector('#sname');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const btn = document.querySelector('.nice');
+
+btn.addEventListener('click', e=>{
+  e.preventDefault()
+  if(nome.vale<3){
+    
+  }
+  
+})
