60c66c3744b50a6a14b12677eb7bfcd641eaee31
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-01-10 20:01:02 -0300

    feat

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,38 @@ const btn = document.querySelector('.nice');
 
 btn.addEventListener('click', e=>{
   e.preventDefault()
-  if(nome.vale<3){
-    
-  }
+  console.log('cheguei aqui e,e')
+ validanome()
+ validasname()
+ validaEmail()
+ validaSenha()
   
 })
+validanome = e=>{
+  if(nome.value.length < 3){
+    const pname = document.querySelector('.pname')
+    pname.innerHTML='Nome não pode ter  menos doque 3 caracteres'
+    pname.classList.remove('invi')
+    }
+}
+validasname = e=>{
+  if(snome.value.length <3){
+    const psname = document.querySelector('.psname')
+    psname.innerHTML = 'Sobrenome não pode ter menos doque 3 caracteres'
+    psname.classList.remove('invi')
+  }
+}
+validaEmail =e =>{
+  if(email.value.length <9){
+    const pemail = document.querySelector('.pemail');
+    pemail.innerHTML = 'Email não pode ter menos de 9 caracteres'
+    pemail.classList.remove('invi')
+  }
+}
+validaSenha = e=>{
+  if(senha.value.length<5){
+    const psenha = document.querySelector('.psenha');
+    psenha.innerHTML = 'Senha não pode ser menor doque 5 caracteres'
+    psenha.classList.remove('invi')
+  }
+}
